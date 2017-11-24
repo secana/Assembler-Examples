137dd463190387e531008df5dee0d8ab6b8873f3
secana <stefan.hausotte@gmx.de>
2017-11-24 20:06:14 +0100

    added x32 hello world with invoke

---
+++ FASM/x32/console_helloworld_invoke.asm
@@ -0,0 +1,23 @@
+format PE console
+entry start
+include 'win32a.inc'
+
+section '.idata' import data readable writeable
+    library kernel32, 'kernel32.dll', \
+        msvcrt, 'MSVCRT.DLL'
+
+    import kernel32, \
+        ExitProcess, 'ExitProcess'
+
+    import msvcrt, \
+        printf, 'printf'
+
+section '.data' data readable
+    szHelloWorld db 'Hello World',0
+
+section '.code' code readable executable
+    start:
+        invoke printf, szHelloWorld
+
+    exit:
+        invoke ExitProcess, 0
